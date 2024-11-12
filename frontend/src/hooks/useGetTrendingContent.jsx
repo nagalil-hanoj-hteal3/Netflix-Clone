2bb08cff01c90d73d646f19612b16237a1512579
EJ Lilagan <elaeth.jonah.lilagan@gmail.com>
2024-11-11 22:04:27 -0800

    added a new change called Next to go to a new movie or tv show when directed in the home page

--- frontend/src/hooks/useGetTrendingContent.jsx
+++ frontend/src/hooks/useGetTrendingContent.jsx
@@ -2,19 +2,22 @@ import { useEffect, useState } from "react";
 import { useContentStore } from "../store/content";
 import axios from "axios";
 
-const useGetTrendingContent = () => {
-	const [trendingContent, setTrendingContent] = useState(null);
-	const { contentType } = useContentStore();
+const useGetTrendingContent = (page = 1) => {
+    const [trendingContent, setTrendingContent] = useState(null);
+    const { contentType } = useContentStore();
 
-	useEffect(() => {
-		const getTrendingContent = async () => {
-			const res = await axios.get(`/api/v1/content/${contentType}/trending`)
-			setTrendingContent(res.data.content);
-		};
+    useEffect(() => {
+        const getTrendingContent = async () => {
+            const res = await axios.get(`/api/v1/content/${contentType}/trending`, {
+                params: { page }  // Pass page as a query parameter
+            });
+            setTrendingContent(res.data.content);
+        };
 
-		getTrendingContent();
-	}, [contentType]);
+        getTrendingContent();
+    }, [contentType, page]);
 
-	return { trendingContent };
+    return { trendingContent };
 };
+
 export default useGetTrendingContent;
